7d59af70692446a812e5604b33d9902faa3f131e
SeanHaugen <sean.haugen560@gmail.com>
2023-08-23 09:57:15 -0500

    flat rate

--- client/src/components/main/itemsPage/flat_rate_shipping/FlatRate.js
+++ client/src/components/main/itemsPage/flat_rate_shipping/FlatRate.js
@@ -2,20 +2,26 @@ import React, { useState } from "react";
 import { useFlatRateInfo } from "../../../../api/api";
 
 function FlatRateShipping({ productData }) {
-  const [itemRates, setItemRates] = useState("");
+  const [itemRates, setItemRates] = useState([]);
 
   useFlatRateInfo(setItemRates, productData.Item_Number);
 
   const handleFlatRate = () => {
     return (
       <div className="box">
-        <h1>Flat Rate Available</h1>
+        <h1>Flat Rate</h1>
         <h4>Qty 1</h4>
-        {itemRates.map((items, index) => (
-          <p key={index}>
-            {items.Service}: {items.Rate}
-          </p>
-        ))}
+        {itemRates !== undefined ? (
+          itemRates.map((items, index) => (
+            <div>
+              <p key={index}>
+                {items.Service}: {items.Rate}
+              </p>
+            </div>
+          ))
+        ) : (
+          <p>No flat Rates</p>
+        )}
       </div>
     );
   };
