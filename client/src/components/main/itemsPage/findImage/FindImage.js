63fded243f4eb48ffb115e374e5c60476bf6e970
SeanHaugen <sean.haugen560@gmail.com>
2023-08-29 07:55:46 -0500

    updated find images

--- client/src/components/main/itemsPage/findImage/FindImage.js
+++ client/src/components/main/itemsPage/findImage/FindImage.js
@@ -1,16 +1,34 @@
+// import { image } from "../../../../../../../../../OneDrive - Sign-Zone Inc/Pictures/images/01Products";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 function FindImage({ productData, category, subCategory }) {
-  const imageFolder = "images/01Products";
+  const [imageURL, setImageURL] = useState("");
 
-  const imageName = `${productData.Item_Number}_0.jpg`;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const imageFolder =
+          "https://signzoneinc.sharepoint.com/sites/customerservicedatabase/shared%20documents/general/images/01products";
+        const imageName = `${productData.Item_Number}_0.jpg?web=1`;
 
-  const imagePath = `/${imageFolder}/${imageName}`;
+        const imageURL = `${imageFolder}/${imageName}`;
+        setImageURL(imageURL);
+      } catch (error) {
+        console.error("error fetching image", error);
+      }
+    };
+    fetchData();
+  }, [productData.Item_Number]);
+
+  console.log(imageURL);
 
   return (
     <div>
       <figure>
         <img
           className="item_image"
-          src={imagePath}
+          src={imageURL}
           alt="Tradeshow display image"
           style={{
             maxWidth: "350px",
